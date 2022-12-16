9b0d060661a4b09c234d5e896ca32d9ea1bf522b
ChoiYoungHoon <dudgns2865@naver.com>
2022-12-16 12:03:08 +0900

    feat(NullOrBlankValidateChain): 빈 값 들어오는 경우 유효성 검증 구현

---
+++ src/main/java/pairmatching/domain/validation/validator/NullOrBlankValidateChain.java
@@ -0,0 +1,28 @@
+package pairmatching.domain.validation.validator;
+
+import pairmatching.domain.pair.dto.InputValidationRequest;
+import pairmatching.domain.pair.dto.InputValidationResponse;
+import pairmatching.domain.pair.type.ValidationType;
+import pairmatching.domain.validation.InputValidationChain;
+
+public class NullOrBlankValidateChain implements InputValidationChain {
+
+    private InputValidationChain next;
+    private static final String NULL_OR_BLANK_MESSAGE = "빈 값은 입력할 수 없습니다.";
+
+    @Override
+    public void setNext(InputValidationChain next) {
+        this.next = next;
+    }
+
+    @Override
+    public InputValidationResponse validate(InputValidationRequest request) {
+        if (!request.getValidationTypes().contains(ValidationType.NULL_OR_BLANK)) {
+            return next.validate(request);
+        }
+        if (request.getTarget() == null || request.getTarget().isBlank()) {
+            return new InputValidationResponse(false, NULL_OR_BLANK_MESSAGE);
+        }
+        return next.validate(request);
+    }
+}
