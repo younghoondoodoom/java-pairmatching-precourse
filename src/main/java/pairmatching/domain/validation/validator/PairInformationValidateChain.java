28d1a355b9b8845d5eee48d190902e15c72ad954
ChoiYoungHoon <dudgns2865@naver.com>
2022-12-16 12:06:10 +0900

    feat(PairInformationValidateChain): 페어 정보 입력 값 유효성 검증 구현

---
+++ src/main/java/pairmatching/domain/validation/validator/PairInformationValidateChain.java
@@ -0,0 +1,28 @@
+package pairmatching.domain.validation.validator;
+
+import pairmatching.domain.pair.dto.InputValidationRequest;
+import pairmatching.domain.pair.dto.InputValidationResponse;
+import pairmatching.domain.pair.type.ValidationType;
+import pairmatching.domain.validation.InputValidationChain;
+
+public class PairInformationValidateChain implements InputValidationChain {
+
+    private InputValidationChain next;
+    private static final String INVALID_SPACE_MESSAGE = "띄어쓰기로 구분 해주세요";
+
+    @Override
+    public void setNext(InputValidationChain next) {
+        this.next = next;
+    }
+
+    @Override
+    public InputValidationResponse validate(InputValidationRequest request) {
+        if (!request.getValidationTypes().contains(ValidationType.PAIR_INFORMATION)) {
+            return next.validate(request);
+        }
+        if (request.getTarget().split(" ").length != 3) {
+            return new InputValidationResponse(false, INVALID_SPACE_MESSAGE);
+        }
+        return next.validate(request);
+    }
+}
